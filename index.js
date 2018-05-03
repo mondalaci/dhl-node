057885468f53f10cd1e1508741084c7ef0489191
László Monda <laci@monda.hu>
2018-05-04 01:00:08 +0200

    Extract wsdlRequest.

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ const soap = require('soap');
 const format = require('xml-formatter');
 const lookup = require('country-data').lookup;
 
-module.exports = {
-    shippingRequest: function(url, auth, req) {
-        return new Promise((resolve, reject) => {
-            const res = {};
-            soap.createClient(url, function(err, client) {
-                if (auth.username === undefined) {
-                    reject('No username specified');
-                }
+function wsdlRequest(wsdlUrl, method, auth, req) {
+    return new Promise((resolve, reject) => {
+        const res = {};
+        soap.createClient(wsdlUrl, function(err, client) {
+            if (auth.username === undefined) {
+                reject('No username specified');
+            }
 
-                if (auth.password === undefined) {
-                    reject('No password specified');
-                }
+            if (auth.password === undefined) {
+                reject('No password specified');
+            }
 
-                const wsSecurity = new soap.WSSecurity(auth.username, auth.password)
-                client.setSecurity(wsSecurity);
+            const wsSecurity = new soap.WSSecurity(auth.username, auth.password)
+            client.setSecurity(wsSecurity);
 
-                client.on('response', responseXml => {
-                    res.responseXml = responseXml;
-                });
-
-                client.createShipmentRequest(req, function(err, response) {
-                    if (err) {
-                        reject(err);
-                    }
-                    res.response = response;
-                    resolve(res);
-                });
-
-                res.requestXml = format(client.lastRequest);
+            client.on('response', responseXml => {
+                res.responseXml = responseXml;
             });
+
+            client[method](req, function(err, response) {
+                if (err) {
+                    reject(err);
+                }
+                res.response = response;
+                resolve(res);
+            });
+
+            res.requestXml = format(client.lastRequest);
         });
+    });
+}
+
+module.exports = {
+    shippingRequest: function(wsdlUrl, auth, req) {
+        return wsdlRequest(wsdlUrl, 'createShipmentRequest', auth, req);
     },
     getIsoDateTime: function() {
         const d = new Date();
