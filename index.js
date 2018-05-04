33abd6459d4fa073319d078c2c9642632627d873
László Monda <laci@monda.hu>
2018-05-04 02:35:33 +0200

    Rename d to date.

--- index.js
+++ index.js
@@ -55,20 +55,20 @@ module.exports = {
         return (new Date).toISOString();
     },
     getIsoDateTimeGmt: function() {
-        const d = new Date();
-        const offset = d.getTimezoneOffset();
+        const date = new Date();
+        const offset = date.getTimezoneOffset();
         const offsetAbs = Math.abs(offset);
         const offsetSign = offset / offsetAbs;
         const offsetSignChar = offsetSign < 0 ? '-' : '+';
         const offsetHoursAbs = Math.floor(offsetAbs / 60);
         const offsetHoursSigned = offsetSign * offsetHoursAbs;
         const offsetMinutesAbs = offsetAbs % 60;
-        return `${d.getUTCFullYear()}-\
-${(d.getUTCMonth()+1).toString().padStart(2, 0)}-\
-${d.getUTCDate().toString().padStart(2,0)}T\
-${d.getUTCHours().toString().padStart(2,0)}:\
-${d.getUTCMinutes().toString().padStart(2,0)}:\
-${d.getUTCSeconds().toString().padStart(2,0)}GMT\
+        return `${date.getUTCFullYear()}-\
+${(date.getUTCMonth()+1).toString().padStart(2, 0)}-\
+${date.getUTCDate().toString().padStart(2,0)}T\
+${date.getUTCHours().toString().padStart(2,0)}:\
+${date.getUTCMinutes().toString().padStart(2,0)}:\
+${date.getUTCSeconds().toString().padStart(2,0)}GMT\
 ${offsetSignChar}\
 ${offsetHoursAbs.toString().padStart(2,0)}:\
 ${offsetMinutesAbs.toString().padStart(2,0)}`;
