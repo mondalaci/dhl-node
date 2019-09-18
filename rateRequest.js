57edb29610d2f909f6970dabfe9143a12ef077d3
László Monda <laci@monda.hu>
2019-09-18 15:10:58 +0200

    Fix indentation and add commas.

--- rateRequest.js
+++ rateRequest.js
@@ -5,47 +5,47 @@ const auth = require('./auth');
 const dhl = require('./index');
 
 const req = {
-        ClientDetail: {
+    ClientDetail: {
+    },
+    RequestedShipment: {
+        DropOffType: 'REQUEST_COURIER',
+        Ship: {
+            Shipper: {
+                StreetLines: '1-16-24, Minami-gyotoku',
+                City: 'Ichikawa-shi, Chiba',
+                PostalCode: '272-0138',
+                CountryCode: 'JP',
+            },
+            Recipient: {
+                StreetLines: '63 RENMIN LU, QINGDAO SHI',
+                City: 'QINGDAO SHI',
+                PostalCode: '266033',
+                CountryCode: 'CN',
+            },
         },
-        RequestedShipment: {
-            DropOffType: 'REQUEST_COURIER',
-            Ship: {
-                Shipper: {
-                    StreetLines: '1-16-24, Minami-gyotoku',
-                    City: 'Ichikawa-shi, Chiba',
-                    PostalCode: '272-0138',
-                    CountryCode: 'JP',
+        Packages: {
+            RequestedPackages: {
+                attributes: {
+                    number: 1,
                 },
-                Recipient: {
-                    StreetLines: '63 RENMIN LU, QINGDAO SHI',
-                    City: 'QINGDAO SHI',
-                    PostalCode: '266033',
-                    CountryCode: 'CN',
-                }
+                Weight: {
+                    Value: 2,
+                },
+                Dimensions: {
+                    Length: 13,
+                    Width: 12,
+                    Height: 9,
+                },
             },
-            Packages: {
-                RequestedPackages: {
-                    attributes: {
-                        number: 1,
-                    },
-                    Weight: {
-                        Value: 2
-                    },
-                    Dimensions: {
-                        Length: 13,
-                        Width: 12,
-                        Height: 9
-                    }
-                }
-            },
-            ShipTimestamp: '2018-02-14T12:00:00GMT-06:00',
-            UnitOfMeasurement: 'SU',
-            Content: 'NON_DOCUMENTS',
-            DeclaredValue: 200,
-            DeclaredValueCurrecyCode: 'USD',
-            PaymentInfo: 'DDP',
-            Account: auth.accountNumber
         },
+        ShipTimestamp: '2018-02-14T12:00:00GMT-06:00',
+        UnitOfMeasurement: 'SU',
+        Content: 'NON_DOCUMENTS',
+        DeclaredValue: 200,
+        DeclaredValueCurrecyCode: 'USD',
+        PaymentInfo: 'DDP',
+        Account: auth.accountNumber,
+    },
 };
 
 (async function() {
