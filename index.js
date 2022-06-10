71f8d49790c5a308e8e1f4dd8a33a33abc180c1f
László Monda <laci@monda.hu>
2022-06-10 17:02:47 +0200

    Convert package to ESM.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const soap = require('soap');
-const format = require('xml-formatter');
-const lookup = require('country-data').lookup;
+import soap from 'soap';
+import format from 'xml-formatter';
+import countryData from 'country-data';
 
 async function wsdlRequest(wsdlUrl, method, auth, req) {
     return new Promise((resolve, reject) => {
@@ -46,44 +46,52 @@ const testUrlPrefix = 'https://wsbexpress.dhl.com/sndpt';
 const liveExpressRateBookUrl = `${liveUrlPrefix}/expressRateBook?WSDL`;
 const testExpressRateBookUrl = `${testUrlPrefix}/expressRateBook?WSDL`;
 
-module.exports = {
-    rateRequest: async function(auth, req) {
-        const res = await wsdlRequest(liveExpressRateBookUrl, 'getRateRequest', auth, req);
-        return res;
-    },
-    requestPickup: async function(auth, req) {
-        return await wsdlRequest(`${liveUrlPrefix}/requestPickup?WSDL`, 'PickUpRequest', auth, req);
-    },
-    shipmentRequest: async function(auth, req) {
-        return await wsdlRequest(liveExpressRateBookUrl, 'createShipmentRequest', auth, req);
-    },
-    trackingRequest: async function(auth, req) {
-        return await wsdlRequest(`${liveUrlPrefix}/glDHLExpressTrack?WSDL`, 'trackShipmentRequest', auth, req);
-    },
-    testRateRequest: async function(auth, req) {
-        return await wsdlRequest(testExpressRateBookUrl, 'getRateRequest', auth, req);
-    },
-    testRequestPickup: async function(auth, req) {
-        return await wsdlRequest(`${testUrlPrefix}/requestPickup?WSDL`, 'PickUpRequest', auth, req);
-    },
-    testShipmentRequest: async function(auth, req) {
-        return await wsdlRequest(testExpressRateBookUrl, 'createShipmentRequest', auth, req);
-    },
-    testTrackingRequest: async function(auth, req) {
-        return await wsdlRequest(`${testUrlPrefix}/glDHLExpressTrack?WSDL`, 'trackShipmentRequest', auth, req);
-    },
-    getIsoDateTime: function() {
-        return (new Date).toISOString();
-    },
-    getIsoDateTimeGmt: function(dateParam) {
-        const date = dateParam || new Date();
-        const offset = date.getTimezoneOffset();
-        const offsetAbs = Math.abs(offset);
-        const offsetSign = offset / offsetAbs;
-        const offsetSignChar = offsetSign < 0 ? '-' : '+';
-        const offsetHoursAbs = Math.floor(offsetAbs / 60);
-        const offsetMinutesAbs = offsetAbs % 60;
-        return `${date.getUTCFullYear()}-\
+export async function rateRequest(auth, req) {
+    const res = await wsdlRequest(liveExpressRateBookUrl, 'getRateRequest', auth, req);
+    return res;
+};
+
+export async function requestPickup(auth, req) {
+    return await wsdlRequest(`${liveUrlPrefix}/requestPickup?WSDL`, 'PickUpRequest', auth, req);
+};
+
+export async function shipmentRequest(auth, req) {
+    return await wsdlRequest(liveExpressRateBookUrl, 'createShipmentRequest', auth, req);
+};
+
+export async function trackingRequest(auth, req) {
+    return await wsdlRequest(`${liveUrlPrefix}/glDHLExpressTrack?WSDL`, 'trackShipmentRequest', auth, req);
+};
+
+export async function testRateRequest(auth, req) {
+    return await wsdlRequest(testExpressRateBookUrl, 'getRateRequest', auth, req);
+};
+
+export async function testRequestPickup(auth, req) {
+    return await wsdlRequest(`${testUrlPrefix}/requestPickup?WSDL`, 'PickUpRequest', auth, req);
+};
+
+export async function testShipmentRequest(auth, req) {
+    return await wsdlRequest(testExpressRateBookUrl, 'createShipmentRequest', auth, req);
+};
+
+export async function testTrackingRequest(auth, req) {
+    return await wsdlRequest(`${testUrlPrefix}/glDHLExpressTrack?WSDL`, 'trackShipmentRequest', auth, req);
+};
+
+export function getIsoDateTime() {
+    return (new Date).toISOString();
+};
+
+export function getIsoDateTimeGmt(dateParam) {
+    const date = dateParam || new Date();
+    const offset = date.getTimezoneOffset();
+    const offsetAbs = Math.abs(offset);
+    const offsetSign = offset / offsetAbs;
+    const offsetSignChar = offsetSign < 0 ? '-' : '+';
+    const offsetHoursAbs = Math.floor(offsetAbs / 60);
+    const offsetMinutesAbs = offsetAbs % 60;
+    return `${date.getUTCFullYear()}-\
 ${(date.getUTCMonth()+1).toString().padStart(2, 0)}-\
 ${date.getUTCDate().toString().padStart(2,0)}T\
 ${date.getUTCHours().toString().padStart(2,0)}:\
@@ -92,14 +100,15 @@ ${date.getUTCSeconds().toString().padStart(2,0)}GMT\
 ${offsetSignChar}\
 ${offsetHoursAbs.toString().padStart(2,0)}:\
 ${offsetMinutesAbs.toString().padStart(2,0)}`;
-    },
-    getMessageReference: function() {
-        return Array(32).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
-    },
-    countryToCode: function(country) {
-        if (country === 'Vietnam') {
-            country = 'Viet Nam';
-        }
-        return lookup.countries({name: country})[0].alpha2;
+};
+
+export function getMessageReference() {
+    return Array(32).fill(0).map(x => Math.random().toString(36).charAt(2)).join('');
+};
+
+export function countryToCode(country) {
+    if (country === 'Vietnam') {
+        country = 'Viet Nam';
     }
+    return countryData.lookup.countries({name: country})[0].alpha2;
 };
